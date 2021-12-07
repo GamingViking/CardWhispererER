8767a7fcfcccc95735288f9da10e224afae0e161
Chris Schmidt <cmschmidt12345@gmail.com>
2021-12-07 11:48:29 -0500

    Three working buttons with sounds, some styling, button tag cannot be styled

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React, {useEffect} from 'react';
 import Sound from 'react-native-sound';
-import { ImageBackground, TouchableOpacity, StyleSheet, Image, Text, View, TouchableOpacityBase } from 'react-native';
+import { ImageBackground, TouchableOpacity, StyleSheet, Image, Text, View, TouchableOpacityBase, Button } from 'react-native';
 
 const styles = StyleSheet.create({
 container: {
@@ -10,17 +10,28 @@ flex: 1,
 alignItems: 'center',
 justifyContent: 'center',
 backgroundColor: '#000',
+borderRadius: 15,
+borderColor: 'orange',
+borderStyle: 'solid',
+borderWidth: 20,
+margin: 20,
 },
 playBtn: {
-  padding: 20,
-  margin: 10,
-  height: 50,
-  width: 50,
-  backgroundColor: 'red',
+  padding: 0,
+  margin: 20,
+  height: 0,
+  width: 0,
+  borderLeftWidth: 30,
+  borderLeftColor: 'transparent',
+  borderRightWidth: 30,
+  borderRightColor: 'transparent',
+  borderBottomWidth: 40,
+  borderBottomColor: 'red',
+  //backgroundColor: 'red',
 },
   playBtn2: {
-    padding: 20,
-    margin: 10,
+    padding: 0,
+    margin: 20,
     height: 50,
     width: 50,
     borderRadius: 25,
@@ -29,6 +40,9 @@ playBtn: {
 picture: {
   flex: 1,
   justifyContent: "center",
+},
+button: {
+  borderRadius: 25,
 },
 });
 
@@ -43,20 +57,21 @@ height: 190,
 Sound.setCategory('Playback');
 
 var testSound = new Sound('aceofspadesaudio.mp3', Sound.MAIN_BUNDLE, () => {
-}
-);
+});
 
 var testSound2 = new Sound('sample3s.mp3', Sound.MAIN_BUNDLE, () => {
-}
-);
+});
+
+var testSound3 = new Sound('kickbass.mp3', Sound.MAIN_BUNDLE, () => {
+});
 
 const App = () => {
-  useEffect(() => {
-  //   testSound.setVolume(1);
-    return () => {
-      testSound.release();
-    };
-  }, []);
+  // useEffect(() => {
+  // //   testSound.setVolume(1);
+  //   return () => {
+  //     testSound.release();
+  //   };
+  // }, []);
   const playPause = () => {
     testSound.play(success => {
       // if (success) {
@@ -70,12 +85,17 @@ const App = () => {
     testSound2.play(success => {
     });
   };
+  const play3 = () => {
+    testSound3.play(success => {
+    });
+  };
   return (
     <View style={styles.container}>
         <TouchableOpacity style={styles.playBtn} onPress={playPause}>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.playBtn2} onPress={play2}>
+        <TouchableOpacity style={styles.playBtn2} color="yellow" onPress={play2}>
         </TouchableOpacity>
+        <Button title="Press" color="purple" onPress={play3} />
     </View>
   )
 };
